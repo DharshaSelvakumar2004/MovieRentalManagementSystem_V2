8f67eed5b5c4d4773e17e75a934b331e9ed1105b
ut01267 <ut01267@UNICOMTIC3>
2024-10-11 12:02:05 +0530

    Add project files.

---
+++ MovieRentalManagementSystem_V2/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalManagementSystem_V2
+{
+    internal class Car
+    {
+        public string MovieId { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public decimal RentalPrice { get; set; }
+    }
+}
